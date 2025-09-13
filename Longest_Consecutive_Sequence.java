f793bed0c32d5a77030273fd5f3034f06fc23707
Krishna-Tayal <161112930+Krishna-Tayal@users.noreply.github.com>
2025-09-13 21:33:11 +0530

    Update Longest_Consecutive_Sequence.java

--- Longest_Consecutive_Sequence.java
+++ Longest_Consecutive_Sequence.java
@@ -4,23 +4,25 @@ You must write an algorithm that runs in O(n) time.*/
 
 class Solution {
     public int longestConsecutive(int[] nums) {
-       Set<Integer> set= new HashSet<>();
-       for(int num : nums) {
-        set.add(num);
-       }
-       int longest=0;
-       int res=0;
-       for(int num : nums){
-         if(!set.contains(num-1)){
-            int current= num;
-            res=1;
+        if (nums == null || nums.length == 0) return 0;
 
-            while(set.contains(current+1)){
-                current++;
-                res++;
+        Set<Integer> set = new HashSet<>();
+        for (int n : nums) set.add(n);
+
+        int maxLen = 0;
+
+        for (int n : set) {              
+            if (!set.contains(n - 1)) {     
+                int current = n;
+                int len = 1;
+                while (set.contains(current + 1)) {
+                    current++;
+                    len++;
+                }
+                if (len > maxLen) maxLen = len;
             }
-         }
-       }
-       return Math.max(res,longest);
+        }
+
+        return maxLen;
     }
 }
