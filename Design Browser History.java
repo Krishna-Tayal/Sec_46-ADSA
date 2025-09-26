ede5853d49b54a518ee24ccef52243b106129f1a
Krishna-Tayal <161112930+Krishna-Tayal@users.noreply.github.com>
2025-09-26 09:18:04 +0530

    Create  Design Browser History.java

---
+++ Design Browser History.java
@@ -0,0 +1,65 @@
+/*Input:
+["BrowserHistory","visit","visit","visit","back","back","forward","visit","forward","back","back"]
+[["leetcode.com"],["google.com"],["facebook.com"],["youtube.com"],[1],[1],[1],["linkedin.com"],[2],[2],[7]]
+Output:
+[null,null,null,null,"facebook.com","google.com","facebook.com",null,"linkedin.com","google.com","leetcode.com"]
+
+Explanation:
+BrowserHistory browserHistory = new BrowserHistory("leetcode.com");
+browserHistory.visit("google.com");       // You are in "leetcode.com". Visit "google.com"
+browserHistory.visit("facebook.com");     // You are in "google.com". Visit "facebook.com"
+browserHistory.visit("youtube.com");      // You are in "facebook.com". Visit "youtube.com"
+browserHistory.back(1);                   // You are in "youtube.com", move back to "facebook.com" return "facebook.com"
+browserHistory.back(1);                   // You are in "facebook.com", move back to "google.com" return "google.com"
+browserHistory.forward(1);                // You are in "google.com", move forward to "facebook.com" return "facebook.com"
+browserHistory.visit("linkedin.com");     // You are in "facebook.com". Visit "linkedin.com"
+browserHistory.forward(2);                // You are in "linkedin.com", you cannot move forward any steps.
+browserHistory.back(2);                   // You are in "linkedin.com", move back two steps to "facebook.com" then to "google.com". return "google.com"
+browserHistory.back(7);                   // You are in "google.com", you can move back only one step to "leetcode.com". return "leetcode.com"*/
+
+class BrowserHistory {
+    private static class Node{
+        String url;
+        Node prev,next;
+        Node(String url){
+            this.url=url;
+        }
+    }
+    private Node curr;
+
+    public BrowserHistory(String homepage) {
+        curr = new Node(homepage);
+    }
+    
+    public void visit(String url) {
+        Node node= new Node(url);
+        curr.next=null;
+        node.prev=curr;
+        curr.next=node;
+        curr=node;
+    }
+    
+    public String back(int steps) {
+        while(steps>0 && curr.prev!=null){
+            curr=curr.prev;
+            steps--;
+        }
+        return curr.url;
+    }
+    
+    public String forward(int steps) {
+        while(steps>0 && curr.next!=null){
+            curr=curr.next;
+            steps--;
+        }
+        return curr.url;
+    }
+}
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory obj = new BrowserHistory(homepage);
+ * obj.visit(url);
+ * String param_2 = obj.back(steps);
+ * String param_3 = obj.forward(steps);
+ */
